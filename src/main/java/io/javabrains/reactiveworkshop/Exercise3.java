fe23f4961cf533dfa4c447b58e0cf0e4e187b3b6
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2022-10-28 17:28:38 +0200

    Exercise 3

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,9 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
@@ -10,11 +13,12 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
+        Mono<List<Integer>> listMono = ReactiveSources.intNumbersFlux().collectList();
 
+        System.out.printf("%s", listMono.block());
 
-        System.out.println("Press a key to end");
-        System.in.read();
+//        System.out.println("Press a key to end");
+//        System.in.read();
     }
 
 }
