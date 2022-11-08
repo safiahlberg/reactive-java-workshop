95588e6ffccd17d7d3f97ac503489e0ab2bd4949
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2022-11-08 23:42:19 +0100

    Exercise 9

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -12,14 +12,21 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        /*ReactiveSources.intNumbersFluxWithException()
+//                .doOnError(throwable -> System.out.printf("Error: %s%n", throwable.getMessage()))
+                .subscribe(num -> System.out.printf("Value: %d%n", num),
+                        err -> System.out.printf("Error: %s%n", err.getMessage()));*/
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+        /*ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue((err, num) -> System.out.printf("Error: %s, for value: %d%n", err.getMessage(), num))
+                .subscribe(num -> System.out.printf("Value: %s%n", num));*/
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume(err -> Flux.just(-1, -2))
+                .subscribe(num -> System.out.printf("Value: %d%n", num));
 
         System.out.println("Press a key to end");
         System.in.read();
