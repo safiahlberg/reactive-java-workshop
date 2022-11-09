3bc5805f9fb22f48a7e00d48b90580522f3e7c14
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2022-11-09 20:45:18 +0100

    Finished Exercise 6

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -12,11 +12,11 @@ public class Exercise6 {
 
         // Get the value from the Mono into a String variable but give up after 5 seconds
         System.out.println("Waiting for response...");
-        String response = ReactiveSources.unresponsiveMono().block(Duration.ofSeconds(5));
+//        String response = ReactiveSources.unresponsiveMono().block(Duration.ofSeconds(5));
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
-        // TODO: Write code here
+        System.out.printf("Values: %s%n", ReactiveSources.unresponsiveFlux().blockLast(Duration.ofSeconds(5)));
 
         System.out.println("Press a key to end");
         System.in.read();
