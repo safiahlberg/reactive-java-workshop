8e68cc34465767e35bde6fadfc948639c6045047
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2022-11-09 20:18:52 +0100

    Exercise 9

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -10,13 +10,27 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+        /*ReactiveSources.intNumbersFlux()
+                .count()
+                .subscribe(num -> System.out.printf("Number of items: %d%n", num));*/
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        /*ReactiveSources.intNumbersFlux()
+                .collectList()
+                .subscribe(intList -> System.out.printf("Items: %s%n", intList));*/
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        /*ReactiveSources.intNumbersFlux()
+                .buffer(2)
+                .filter(nums -> nums.size() == 2)
+                .map(nums -> nums.get(0) + nums.get(1))
+                .subscribe(num -> System.out.printf("Value: %d%n", num));*/
+
+        // Transforma to a sequence of sums of adjacent two numbers, but only advance one step at the time
+        ReactiveSources.intNumbersFlux()
+                .zipWith(ReactiveSources.intNumbersFlux().skip(1))
+                .map(tuple -> tuple.getT1() + tuple.getT2())
+                .subscribe(num -> System.out.printf("Value: %d%n", num));
 
         System.out.println("Press a key to end");
         System.in.read();
